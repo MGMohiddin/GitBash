901b1192ce0c6a7228d69b7ef66cee1feef03bbb
MGMohiddin <gouse.mohiddin1996@gmail.com>
2021-10-29 12:10:08 +0530

    My Second commit...

--- src/javaProject/Sample1.java
+++ src/javaProject/Sample1.java
@@ -4,6 +4,8 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		System.out.println("My First Project");
+		
+		System.out.println("Manisha Pal Second project commit...");
 	}
 
 }
